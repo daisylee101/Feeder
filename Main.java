38fab51c3e25eb6b7b6458fe0d394541943f2170
daisylee101 <daisyl43@nycstudents.net>
2024-09-27 16:35:30 +0000

    Updates

--- Main.java
+++ Main.java
@@ -3,17 +3,24 @@ public class Main
     public static void main(String[] args)
     {
         Feeder f = new Feeder(500);
-        System.out.println(f);
+       // System.out.println(f);
         //System.out.println(f.currentFood);
-        System.out.println(f.getCurrentFood());
+       // System.out.println(f.getCurrentFood());
         Feeder g = new Feeder(500);
-        System.out.println(g.getCurrentFood());
-        System.out.println(Math.random());
+       // System.out.println(g.getCurrentFood());
+       // System.out.println(Math.random());
         int random = (int)(Math.random() * 50 + 1);
-        System.out.println(random);
-        System.out.println(Math.random() < 0.05);
+       // System.out.println(random);
+       // System.out.println(Math.random() < 0.05);
         g.simulateOneDay(12);
         System.out.println(g.getCurrentFood());
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
+
+        Feeder j = new Feeder(2400);
+        System.out.println(j.simulateManyDays(10, 4));
+        
 
 
     }
